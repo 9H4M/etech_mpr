fae93b6a46bbb8be0e206dec6569898b0846e9cd
Hani Mahmoud <Hani.Mahmoud.49@outlook.com>
2025-12-22 14:36:47 +0300

    init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "E-Tech Material Purchase Requisition Customization",
+    "version": "12.0.1.0.0",
+    "category": "Purchase",
+    "summary": "Modified Requisition Report for E-Tech",
+    "author": "Hani Emara",
+    "depends": ["material_purchase_requisitions"],
+    "data": [
+        "views/report_purchase_requisition.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
